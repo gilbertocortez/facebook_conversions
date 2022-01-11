554b46678508a058a8ef5b5c5c7185bc5a5c56ac
Gilberto Cortez <me@gilbertocortez.com>
2022-01-10 20:25:57 -0800

    Initial commit

---
+++ inc/objects/curl.class.php
@@ -0,0 +1,48 @@
+<?php
+// ------------------------------------------------
+// This is a cURL Object
+// Created by: Gilberto C.
+// InteractiveUtopia.com
+// ------------------------------------------------
+
+class CurlServer
+{
+    private $access_token;
+
+    function __construct()
+    {
+        $this->access_token = FB_ACCESS_TOKEN;
+    }
+
+    function post_request($url, $submitJson)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $submitJson);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $this->access_token, 'Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+        $serverReponseObject = json_decode($server_output);
+
+        // Debug
+        //print_r ( $server_output );
+        print_r($serverReponseObject);
+    }
+    function get_request($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $this->access_token));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+        $serverReponseObject = json_decode($server_output);
+
+        // Debug
+        //print_r ( $server_output );
+        print_r($serverReponseObject);
+    }
+}
